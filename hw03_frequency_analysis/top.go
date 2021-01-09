94e15517c10a48a68097b4c28406ee9f815f3353
Sergii Rodymenko <sergii.rodymenko@gmail.com>
2021-01-09 17:40:10 +0200

    homework-03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,37 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	reg := regexp.MustCompile(`[!"',. \t\v\r\n\f]+`)
+	textWords := reg.Split(text, -1)
+
+	wordStatMap := map[string]int{}
+
+	for _, word := range textWords {
+		if word == "" || word == "-" {
+			continue
+		}
+		wordStatMap[strings.ToLower(word)]++
+	}
+
+	wordStatSlice := make([]string, 0, len(wordStatMap))
+
+	for word := range wordStatMap {
+		wordStatSlice = append(wordStatSlice, word)
+	}
+
+	sort.Slice(wordStatSlice, func(i, j int) bool {
+		return wordStatMap[wordStatSlice[i]] > wordStatMap[wordStatSlice[j]]
+	})
+
+	if len(wordStatSlice) > 10 {
+		wordStatSlice = wordStatSlice[:10]
+	}
+
+	return wordStatSlice
 }
